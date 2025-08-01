d99f209fff4190f28838fe91e76808083cd57268
Phil Bell <philip.bell@kraken.tech>
2025-08-01 17:28:12 +0100

    refactor

--- copy_path_as_module.py
+++ copy_path_as_module.py
@@ -6,20 +6,19 @@ from os.path import relpath
 class CopyPathAsModule(sublime_plugin.TextCommand):
     def run(self, edit):
         filename = self.view.file_name()
-        if len(filename) > 0:
-            path = min(
-                (relpath(filename, folder) for folder in sublime.active_window().folders()),
-                key=len,
-            )
-            path = path[:-3]
-            path = path.split("/")
-            path.pop(0)
-            file = path.pop(-1)
-            module = path[-1] + "_" + file
-            path = ".".join(path)
-            path = "from " + path + " import " + file + " as " + module
-            sublime.set_clipboard(path)
-            sublime.status_message("Copied path as module")
+        path = min(
+            (relpath(filename, folder) for folder in sublime.active_window().folders()),
+            key=len,
+        )
+        path = path[:-3]
+        path = path.split("/")
+        path.pop(0)
+        file = path.pop(-1)
+        module = path[-1] + "_" + file
+        path = ".".join(path)
+        path = "from " + path + " import " + file + " as " + module
+        sublime.set_clipboard(path)
+        sublime.status_message("Copied path as module")
 
     def is_enabled(self):
         return bool(self.view.file_name() and len(self.view.file_name()) > 0)
--- copy_path_as_pytest.py
+++ copy_path_as_pytest.py
@@ -1,48 +1,52 @@
-import sublime_plugin
 import sublime
+import sublime_plugin
 from os.path import relpath
 
 
 class CopyPathAsPytest(sublime_plugin.TextCommand):
     def run(self, edit):
-        if not self.view.sel():
-            return
-        point = self.view.sel()[0].begin()
+        relative_path = self._get_relative_path()
+        function_name = self._find_current_function_name()
 
-        # 2. Find all regions scoped as a function in the current view
-        # The 'meta.function' scope is standard for many languages
-        function_regions = self.view.find_by_selector("meta.function")
+        command = f"inv localdev.pytest {relative_path} -- -s"
+        if function_name:
+            command += f" -k {function_name}"
 
-        current_function_region = None
-        # 3. Find which function region, if any, contains the cursor
-        for r in function_regions:
-            if r.contains(point):
-                current_function_region = r
-                break
-
-        if not current_function_region:
-            sublime.status_message("Cursor is not inside a known function.")
-            return
-
-        # 4. Within the function region, find the region for the function's name
-        # The 'entity.name.function' scope is the common standard
-        name_regions = self.view.find_by_selector("entity.name.function")
-
-        current_function_name = "Unknown Function"
-        for name_region in name_regions:
-            # 5. Check if this name region is inside our located function region
-            if current_function_region.contains(name_region):
-                # 6. Extract the text of the function name
-                current_function_name = self.view.substr(name_region)
-                break
-        filename = self.view.file_name()
-        path = min(
-            (relpath(filename, folder) for folder in sublime.active_window().folders()),
-            key=len,
-        )
-
-        sublime.set_clipboard("inv localdev.pytest " + path + " -- -s -k " + current_function_name)
-        sublime.status_message("Copied test command")
+        sublime.set_clipboard(command)
+        sublime.status_message("Copied pytest command")
 
     def is_enabled(self):
-        return bool(self.view.file_name() and len(self.view.file_name()) > 0)
+        return bool(self.view.file_name())
+
+    def _get_relative_path(self):
+        filename = self.view.file_name()
+        folders = self.view.window().folders()
+
+        if not folders:
+            return filename
+
+        return min((relpath(filename, f) for f in folders), key=len)
+
+    def _find_current_function_name(self):
+        if not self.view.sel():
+            return None
+
+        point = self.view.sel()[0].begin()
+
+        func_region = next(
+            (r for r in self.view.find_by_selector("meta.function") if r.contains(point)), None
+        )
+
+        if not func_region:
+            return None
+
+        name_region = next(
+            (
+                r
+                for r in self.view.find_by_selector("entity.name.function")
+                if func_region.contains(r)
+            ),
+            None,
+        )
+
+        return self.view.substr(name_region) if name_region else None
